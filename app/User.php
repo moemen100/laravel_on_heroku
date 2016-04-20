345d62fd7b46374341d1fc256e4160ac8977f5d0
moemen100 <moemen100@yahoo.com>
2016-04-20 08:04:41 +0200

    add_database

--- app/User.php
+++ app/User.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class user extends Model implements authenticatable
 {
-   use \ Illuminate\Auth\authenticatable; 
+    use \ Illuminate\Auth\authenticatable;
 }
