ec3903de7b2a1dc3cbcdda523c7078daa97eb9fc
moemen100 <moemen100@yahoo.com>
2016-04-24 06:23:58 +0200

    handle some errors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,7 +37,7 @@ Dummy wep
                     @if (!Storage::disk('local')->has($post->user->first_name . '-' . $post->user->id . '.jpg'))
 
                         <div class="col-md-4">
-                            <img src="{{ route('account.image', ['filename' => 'mo-4.jbg']) }}" alt="" class="img-responsive">
+                            <img src="{{ route('account.image', ['filename' => 'mo-4.PNG']) }}" alt="" class="img-responsive">
                         </div>
 
                     @endif
